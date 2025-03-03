f7353e227f264252d72f3be217b7fc219a50ef7d
Mubasshir Ahin <104796739+mubasshirahin@users.noreply.github.com>
2025-03-04 01:14:29 +0600

    6

---
+++ Codeforces/975A(D2).cpp
@@ -0,0 +1,208 @@
+/*
+                                                                                                                                                                                                                                                                                                                                                                                    
+█████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████
+                                                                                                                                                                                                                                                                                                                                                                                     
+     ___       __    __   __  .__   __. 
+    /   \     |  |  |  | |  | |  \ |  | 
+   /  ^  \    |  |__|  | |  | |   \|  | 
+  /  /_\  \   |   __   | |  | |  . `  | 
+ /  _____  \  |  |  |  | |  | |  |\   | 
+/__/     \__\ |__|  |__| |__| |__| \__| 
+
+                                                                                                                                                                                                                                                                                                                                                                                     
+█████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//  ___________________________________________________________________________________________________________________________
+// /___/___/___/___/___/___/___/___/___/___/___/___/___/___/MACROS/___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/
+
+#define pb push_back
+#define fl(i,n) for(int i=0;i<n;i++)
+#define fla(a,n) for(int i=a;i<n;i++)
+#define rfl(i,n,m) for(int i=n;i>=m;i--)
+#define py cout << "YES" << endl;
+#define pn cout << "NO" << endl;
+#define grumpyonion() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//   ___________________________________________________________________________________________________________________________
+// /___/___/___/___/___/___/___/___/___/___/___/___/___/___/ALIASES/___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/
+
+using ll = long long;
+
+//  ______________________________________________________________________________________________________________________________
+// /___/___/___/___/___/___/___/___/___/___/___/___/___/___/CONSTANTS/___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/
+
+const ll lma = LONG_LONG_MAX;
+const ll lmi = LONG_LONG_MIN;
+const double pi = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+//  ____________________________________________________________________________________________________________________________
+// /___/___/___/___/___/___/___/___/___/___/___/___/___/___/TYPEDEF/___/___/___/___/___/___/___/___/___/___/___/___/___/___/___/
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+//  ____________________________________________________________________________________________________________________________
+// /___/___/___/___/___/___/___/___/___/___/___/___/___/___/IMPORTANT FUNCTIONS/___/___/___/___/___/___/___/___/___/___/___/___/
+
+void printArr(int arr[], int size) { fl(i, size) cout << arr[i] << " "; cout << endl; }
+void printVec(vector<ll> v) { int n = v.size(); fl(i, n) cout << v[i] << " "; cout << endl; }
+void scanArr(int arr[], int size) { for (int i = 0; i < size; i++) { cin >> arr[i]; } }
+void scanVec(vector<ll>& v, int size) { for (int i = 0; i < size; i++) { int x; cin >> x; v.push_back(x); } }
+
+//  _______________________________________________________________________________________________________________________________
+// /___/___/___/___/___/___/___/___/___/___/___/___/___/___/MATHEMATICAL FUNCTIONS/___/___/___/___/___/___/___/___/___/___/___/___/
+
+int gcd(int a, int b) { if (a == 0) return b; else return gcd(b % a, a); }
+int lcm(int a, int b) { return std::abs(a * b) / gcd(a, b); }
+int maxThree(int a, int b, int c) { return (a >= b && a >= c) ? a : (b >= c) ? b : c; }
+int maxInArray(int arr[], int size) { int max = INT_MIN; for (int i = 0; i < size; i++) { max = (arr[i] > max) ? arr[i] : max; } return max; }
+int minInArray(int arr[], int size) { int min = INT_MAX; for (int i = 0; i < size; i++) { min = (arr[i] < min) ? arr[i] : min; } return min; }
+int countPrime(int n) { int count = 0; vector<bool> prime(n, true); prime[0] = prime[1] = false; for (int i = 2; i < n - 1; i++) { if (prime[i]) { count++; for (int j = 2 * i; j < n; j += i) { prime[j] = false; } } } return count; }
+int sameFlow(int arr[], int size) { int ans = 0; fl(i, size) { ans = (ans * 10) + arr[i]; } return ans; }
+int reverseFlow(int arr[], int size) { int ans1 = 0; fl(i, size) { ans1 = (arr[i] * pow(10, i)) + ans1; } return ans1; }
+int findUnique(int arr[], int size) { int unique = 0; fl(i, size) { unique ^= arr[i]; } return unique; }
+int findDuplicate(int arr[], int size) { sort(arr, arr + size); for (int i = 1; i < size; ++i) { if (arr[i] == arr[i - 1]) { return arr[i]; } } return -1; }
+void rightRotate(int arr[], int size, int k) { int temp[size]; fl(i, size) { temp[(i + k) % size] = arr[i]; } fl(i, size) { arr[i] = temp[i]; } }
+void leftRotate(int arr[], int size, int k) { int temp[size]; fl(i, size) { temp[(i - k + size) % size] = arr[i]; } fl(i, size) { arr[i] = temp[i]; } }
+void countOccurrences(int arr[], int size) {int count[size] = {0};for (int i = 0; i < size; ++i) {bool alreadyCounted = false;for (int j = 0; j < i; ++j) {if (arr[i] == arr[j]) {alreadyCounted = true;break;}}if (!alreadyCounted) {int occurrences = 0;for (int k = 0; k < size; ++k) {if (arr[k] == arr[i]) {++occurrences;}}count[i] = occurrences;}}printArr(count,size);}
+
+//  __________________________________________________________________________________________________________________________________
+// /___/___/___/___/___/___/___/___/___/___/___/___/___/___/BOOLEAN FUNCTIONS/___/___/___/___/___/___/___/___/___/___/___/___/__/__/__/
+
+bool isPrime(int n) {if (n <= 1) return false;if (n <= 3) return true;if (n % 2 == 0 || n % 3 == 0) return false;for (int i = 5; i * i <= n; i += 6) {if (n % i == 0 || n % (i + 2) == 0) return false;}return true;}
+
+//  __________________________________________________________________________________________________________________________________
+// /___/___/___/___/___/___/___/___/___/___/___/___/___/___/DEBUGGER/___/___/___/___/___/___/___/___/___/___/___/___/__/__/__/___/___/
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+void faltu () {
+    cerr << endl;
+}
+
+template <typename T>
+void faltu( T a[], int n ) {
+    for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+    cerr << arg << ' ';
+    faltu(rest...);
+}
+
+//█████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████
+
+
+int main()
+{
+    grumpyonion();
+    
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        vector<ll> v;
+        scanVec(v,n);
+
+        vector<ll> v1;
+        vector<ll> v2;
+
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                v1.push_back(v[i]);
+            }
+            else{
+                v2.push_back(v[i]);
+            }
+        }
+
+        ll max1 = (v1.empty() ? 0 : *max_element(v1.begin(), v1.end()));
+        ll max2 = (v2.empty() ? 0 : *max_element(v2.begin(), v2.end()));
+
+        // if(max1>=max2){
+        //     cout<<(max1+v1.size())<<endl;
+        // }
+        // else{
+        //     cout<<(max2+v2.size())<<endl;
+        // } 
+
+        ll score1 = max1 + v1.size();
+        ll score2 = max2 + v2.size();
+
+        cout << max(score1, score2) << endl;
+    }
+
+        // printVec(v1);
+        // printVec(v2);
+
+return 0;
+}
