db2e03454242a07f71e7ce8143142557c58aac20
Mubasshir Ahin <104796739+mubasshirahin@users.noreply.github.com>
2025-03-04 01:19:21 +0600

    5

---
+++ Codeforces/972B1(D2).cpp
@@ -0,0 +1,197 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fl(i,n) for(int i=0;i<n;i++)
+#define fla(a,n) for(int i=a;i<n;i++)
+#define rfl(i,n,m) for(int i=n;i>=m;i--)
+#define py cout << "YES" << endl;
+#define pn cout << "NO" << endl;
+#define grumpyonion() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+using ll = long long;
+
+const ll lma = LONG_LONG_MAX;
+const ll lmi = LONG_LONG_MIN;
+const double pi = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+void printArr(int arr[], int size) { fl(i, size) cout << arr[i] << " "; cout << endl; }
+void printVec(vector<int> v) { int n = v.size(); fl(i, n) cout << v[i] << " "; cout << endl; }
+void scanArr(int arr[], int size) { for (int i = 0; i < size; i++) { cin >> arr[i]; } }
+void scanVec(vector<int>& v, int size) { for (int i = 0; i < size; i++) { int x; cin >> x; v.push_back(x); } }
+
+int gcd(int a, int b) { if (a == 0) return b; else return gcd(b % a, a); }
+int lcm(int a, int b) { return std::abs(a * b) / gcd(a, b); }
+int maxThree(int a, int b, int c) { return (a >= b && a >= c) ? a : (b >= c) ? b : c; }
+int countPrime(int n) { int count = 0; vector<bool> prime(n, true); prime[0] = prime[1] = false; for (int i = 2; i < n - 1; i++) { if (prime[i]) { count++; for (int j = 2 * i; j < n; j += i) { prime[j] = false; } } } return count; }
+bool isPrime(int n) { if (n <= 1) return false; if (n <= 3) return true; if (n % 2 == 0 || n % 3 == 0) return false; for (int i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0) return false; } return true; }
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+void faltu () {
+    cerr << endl;
+}
+template <typename T>
+void faltu( T a[], int n ) {
+    for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+    cerr << endl;
+}
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+    cerr << arg << ' ';
+    faltu(rest...);
+}
+
+//**********************************//
+
+int main()
+{
+    grumpyonion();
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,q;
+        cin>>n>>m>>q;
+        vi v;
+        scanVec(v,m);
+        sort(all(v));
+        int x;
+        cin>>x;
+
+        if(x>v[0] && x>v[1]){
+            int l = max(v[0],v[1]);
+            cout<<(n-x)+(x-l)<<endl;
+            continue;    
+        }
+
+        else if(x<v[0] && x<v[1]){
+            x=1;
+            int p = min(v[0],v[1]);
+            cout<<(p-x)<<endl;
+        }
+        
+        else{
+            // int i=v[0];
+            // int j=v[1];
+            // int cnt=0;
+
+            // while(true){
+            //     int d1 = (x-i);
+            //     int d2 = j - x;
+
+            //     if(d1>d2){
+            //         x--;
+            //     }
+            //     else if(d1<d2){
+            //         x++;
+            //     }
+            //     else if(d1==d2){
+            //         continue;
+            //     }
+
+            //     d1 = (x-i);
+            //     d2 = j - x;
+
+            //     if(d1<=d2){
+            //         i++;
+            //         cnt++;
+            //     }
+            //     else{
+            //         j--;
+            //         cnt++;
+            //     }
+
+            //     if(i==x || j==x){
+            //         break;
+            //     }
+            // }
+            // cout<<cnt<<endl;
+
+            
+
+            // int b = (v[1]-v[0])+1;
+            // if(b==3){
+            //     cout<<1<<endl;
+            // }
+            // else if(b>=6){
+            //     cout<<b-4<<endl;
+            // }
+            // else
+            // cout<<b-3<<endl;
+
+            ll mid=(v[0]+v[1])/2;
+            cout<<min(v[1]-mid,mid-v[0])<<endl;
+        }
+
+        
+
+
+
+
+
+    }
+
+return 0;
+}
