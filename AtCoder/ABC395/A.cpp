ac4bd399119e80ed79d720a8def57f40a5aa376f
Mubasshir Ahin <104796739+mubasshirahin@users.noreply.github.com>
2025-03-04 17:47:52 +0600

    atc

--- AtCoder/ABC395/A.cpp
+++ AtCoder/ABC395/A.cpp
@@ -13,20 +13,12 @@ using namespace std;
 #define py cout << "YES" << endl;
 #define pn cout << "NO" << endl;
 
-#define fl(i, n) for (int i = 0; i < n; i++)
-#define fla(a, n) for (int i = a; i < n; i++)
-#define rfl(i, n, m) for (int i = n; i >= m; i--)
-#define grumpyonion()             \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
-#define fraction(a)               \
-    cout.unsetf(ios::floatfield); \
-    cout.precision(a);            \
-    cout.setf(ios::fixed, ios::floatfield);
-#define file()                        \
-    freopen("input.txt", "r", stdin); \
-    freopen("output.txt", "w", stdout);
+#define fl(i,n) for(int i=0;i<n;i++)
+#define fla(a,n) for(int i=a;i<n;i++)
+#define rfl(i,n,m) for(int i=n;i>=m;i--)
+#define grumpyonion() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
 
 using ll = long long;
 using dl = double;
@@ -42,113 +34,42 @@ typedef vector<int> vi;
 typedef vector<ll> vl;
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
-typedef pair<int, int> pii;
+typedef pair<int,int> pii;
 typedef pair<double, double> pdd;
 typedef pair<ll, ll> pll;
 typedef vector<pii> vii;
 typedef vector<pll> vll;
 
-int gcd(int a, int b)
-{
-    if (a == 0)
-        return b;
-    else
-        return gcd(b % a, a);
-}
+int gcd(int a, int b) { if (a == 0) return b; else return gcd(b % a, a); }
 int lcm(int a, int b) { return abs(a * b) / gcd(a, b); }
-int maxThree(int a, int b, int c) { return (a >= b && a >= c) ? a : (b >= c) ? b
-                                                                             : c; }
-int countPrime(int n)
-{
-    int count = 0;
-    vector<bool> prime(n, true);
-    prime[0] = prime[1] = false;
-    for (int i = 2; i < n - 1; i++)
-    {
-        if (prime[i])
-        {
-            count++;
-            for (int j = 2 * i; j < n; j += i)
-            {
-                prime[j] = false;
-            }
-        }
-    }
-    return count;
-}
-bool isPrime(int n)
-{
-    if (n <= 1)
-        return false;
-    if (n <= 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    for (int i = 5; i * i <= n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-    }
-    return true;
-}
+int maxThree(int a, int b, int c) { return (a >= b && a >= c) ? a : (b >= c) ? b : c; }
+int countPrime(int n) { int count = 0; vector<bool> prime(n, true); prime[0] = prime[1] = false; for (int i = 2; i < n - 1; i++)
+{ if (prime[i]) { count++; for (int j = 2 * i; j < n; j += i) { prime[j] = false; } } } return count; }
+bool isPrime(int n) { if (n <= 1) return false; if (n <= 3) return true; if (n % 2 == 0 || n % 3 == 0) return false;
+for (int i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0) return false; } return true; }
 
-void printArr(int arr[], int size)
-{
-    fl(i, size) cout << arr[i] << " ";
-    cout << endl;
-}
-void printVec(vector<int> v)
-{
-    int n = v.size();
-    fl(i, n) cout << v[i] << " ";
-    cout << endl;
-}
-void scanArr(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        cin >> arr[i];
-    }
-}
-void scanVec(vector<int> &v, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        int x;
-        cin >> x;
-        v.push_back(x);
-    }
-}
+void printArr(int arr[], int size) { fl(i, size) cout << arr[i] << " "; cout << endl; }
+void printVec(vector<int> v) { int n = v.size(); fl(i, n) cout << v[i] << " "; cout << endl; }
+void scanArr(int arr[], int size) { for (int i = 0; i < size; i++) { cin >> arr[i]; } }
+void scanVec(vector<int>& v, int size) { for (int i = 0; i < size; i++) { int x; cin >> x; v.push_back(x); } }
 
 /*-----------------------------------------------------------------------------------------------------------*/
 
 int main()
 {
     grumpyonion();
+ 
+    string s;
+    cin>>s;
 
-    int n;
-    cin >> n;
+    string ss;
 
-    int tmp = INT_MIN;
-    vi v;
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        v.push_back(x);
-
-        if (x > tmp)
-        {
-            tmp = x;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='2'){
+            ss.push_back('2');
+            }
         }
-        else
-        {
-            cout << "No" << endl;
-            return 0;
-        }
-    }
 
-    cout << "Yes" << endl;
-
-    return 0;
+        cout<<ss<<endl;
+ return 0;
 }
